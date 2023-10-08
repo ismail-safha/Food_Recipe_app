9f880761f918e154e5647de57c16e0399639ad95
ismail safha <ismailsafha@gmail.com>
2023-10-08 08:33:08 +0100

    Setting up app and install dependencies

---
+++ src/components/recipes.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function recipes() {
+  return (
+    <View>
+      <Text>recipes</Text>
+    </View>
+  );
+}
---
+++ src/helpers/image.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function image() {
+  return (
+    <View>
+      <Text>image</Text>
+    </View>
+  );
+}
